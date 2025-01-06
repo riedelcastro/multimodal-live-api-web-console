0a93e6aec20c11e02a989f1b0b78a2964319bb21
Sebastian Riedel <srriedel@google.com>
2025-01-06 13:00:09 +0000

    Now using iframes and message passing to separate parent and child app

--- src/components/memento/Memento.tsx
+++ src/components/memento/Memento.tsx
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-import { type FunctionDeclaration, SchemaType } from "@google/generative-ai";
+import { type FunctionDeclaration, GenerativeContentBlob, Part, SchemaType } from "@google/generative-ai";
 import { useEffect, useRef, useState, memo, Attributes, useLayoutEffect } from "react";
 import { useLiveAPIContext } from "../../contexts/LiveAPIContext";
 import { ToolCall } from "../../multimodal-live-types";
@@ -21,6 +21,7 @@ import { MultimodalLiveClient } from "../../lib/multimodal-live-client";
 import sanitizeHtml from 'sanitize-html';
 import * as htmlparser2 from "htmlparser2";
 import { DomHandler, DomHandlerOptions, Node, Element, Document, Text } from "domhandler";
+import { Helmet } from "react-helmet";
 
 
 type Dictionary = {
@@ -66,20 +67,54 @@ function sendUserMessage(client: MultimodalLiveClient, text: string) {
   );
 }
 
+function sendUserImage(client: MultimodalLiveClient, data: string) {
+  setTimeout(
+    () => {
+      // const contentBlob: GenerativeContentBlob = {
+      //   mimeType: "image/jpeg",
+      //   data: data
+      // };
+      // const part: Part = { inlineData: contentBlob };
+      // client.send([part], true );
+      // https://github.com/google-gemini/cookbook/blob/main/gemini-2/live_api_starter.py#L76-L119
+      client.sendRealtimeInput([{ mimeType: "image/jpeg", data: data }])
+      // client.sendRealtimeInput([{mimeType:  "audio/pcm;rate=16000", data: ""}])
+
+    },
+    200,
+  );
+}
+
 
 const renderHtmlDeclaration: FunctionDeclaration = {
   name: "render_html",
-  description: "Displays content in html format, embedded in a div element on the current page.",
+  description: "Renders an html page in an iframe.",
   parameters: {
     type: SchemaType.OBJECT,
     properties: {
       htmlText: {
         type: SchemaType.STRING,
         description:
-          "HTML content in string format. The content will be embedded within a div element in the current page.",
+          "HTML page in string format.",
       },
     },
     required: ["htmlText"],
+  },
+};
+
+const sendMessageToIframeDeclaration: FunctionDeclaration = {
+  name: "send_message_to_iframe",
+  description: "Sends a message to the iframe that contains the rendered html. You can receive these messages within the javascript you generate.",
+  parameters: {
+    type: SchemaType.OBJECT,
+    properties: {
+      message: {
+        type: SchemaType.STRING,
+        description:
+          "message to be sent to the iframe.",
+      },
+    },
+    required: ["message"],
   },
 };
 
@@ -157,11 +192,19 @@ const rmFileDeclaration: FunctionDeclaration = {
   },
 };
 
+const oldSystemInstruction = `
+# Rendering HTML
+* The default background is dark, so make sure that you choose appropriate colours. 
+* Inline Javascript in event handlers can send content back to gemini via 'document.getElementById("memento").sendToGemini("<your prompt here>")'.
+* Inline Javascript in event handlers can also send image content back to gemini via 'document.getElementById("memento").sendImageToGemini("<your image in image/jpeg format as base64 string here>")'.
+
+`
 
 const systemInstruction = `
 # Rendering HTML
 * The default background is dark, so make sure that you choose appropriate colours. 
-* Inline Javascript in event handlers can send content back to gemini via 'document.getElementById("memento").sendToGemini("<your prompt here>")'.
+* In the HTML you can use javascript to send messages to Gemini via 'window.parent.postMessage({type: "userResponse", prompt: "<your prompt here>"}, "http://localhost:3000")'; 
+* You can also listen to messages send from Gemini via 'window.addEventListener("message", <your message listener here>)'. The message will have the type: 'modelResponse' and the response will be in the 'response' property.
 `
 
 async function listFilesInCurrentFolder() {
@@ -197,9 +240,30 @@ async function removeFile(name: string) {
   return root.removeEntry(name);
 }
 
+const dummyHtml = `
+<html>
+  <body>
+     Hello World
+     <script>
+        const someVar = 123;
+        console.log("IFrame script called", someVar);
+        console.log("IFrame parent window",window.parent);
+        console.log("Memento", document.getElementById('memento'));
+        console.log("Origin", window.location.origin);
+        window.parent.postMessage('Yo!', "http://localhost:3000/"); 
+        document.addEventListener('DOMContentLoaded', function (event) {
+          console.log("DOM content has loaded");
+          console.log("Doc", document);
+          // console.log("Parent", window.parent.document);
+        })
+     </script>
+  </body>
+</html>
+`
+
 function MementoComponent() {
   const [htmlString, setHtmlString] = useState<string>("");
-  const { client, setConfig } = useLiveAPIContext();
+  const { client, connected, setConfig } = useLiveAPIContext();
   const elRef = useRef<HTMLDivElement>(null);
 
 
@@ -223,13 +287,34 @@ function MementoComponent() {
       tools: [
         // there is a free-tier quota for search
         { googleSearch: {} },
-        { functionDeclarations: [renderHtmlDeclaration, lsDeclaration, writeToDeclaration, readTxtFileDeclaration, rmFileDeclaration] },
+        { functionDeclarations: [renderHtmlDeclaration, lsDeclaration, writeToDeclaration, readTxtFileDeclaration, rmFileDeclaration, sendMessageToIframeDeclaration] },
       ],
     });
   }, [setConfig]);
 
+  function onReceivedMessage(event: MessageEvent) {
+    console.log("Message received: ", event.data);
+    console.log("Event Received: ", event);
+    console.log("Origin", event.origin);
+    if (event.origin == "http://localhost:3000" && event.data.type == "userResponse") {
+      sendUserMessage(client, event.data.prompt);
+      console.log("Sent to gemini");
+    }
+  }
 
-  type MementoElementType = HTMLElement & { sendToGemini?: (prompt: string) => void };
+  useEffect(() => {
+    window.addEventListener("message", onReceivedMessage);
+
+    return () => {
+      window.removeEventListener("message", onReceivedMessage);
+    };
+  })
+
+
+  type MementoElementType = HTMLElement & {
+    sendToGemini?: (prompt: string) => void,
+    sendImageToGemini?: (data: string) => void
+  };
 
   useEffect(() => {
     const memento = document.getElementById("memento") as MementoElementType;
@@ -237,6 +322,10 @@ function MementoComponent() {
       console.log("Client is", client);
       console.log("Prompt is: ", prompt);
       sendUserMessage(client, prompt);
+    }
+    memento.sendImageToGemini = (data: string) => {
+      sendUserImage(client, data);
+      console.log("Image send");
     }
     console.log("Extra function set");
   }, [client])
@@ -249,6 +338,14 @@ function MementoComponent() {
           case renderHtmlDeclaration.name: {
             const args = fc.args as { htmlText: string };
             setHtmlString(args.htmlText);
+            sendToolResponse(client, toolCall, { response: { output: { sucess: true } } });
+            break
+          }
+          case sendMessageToIframeDeclaration.name: {
+            const args = fc.args as { message: string };
+            const iframe = document.getElementById('html_canvas') as HTMLIFrameElement;
+            iframe?.contentWindow?.postMessage({type: "modelResponse", response: args.message}, "*");
+            // window.parent.postMessage({type: "modelResponse", message: args.message}, "*");
             sendToolResponse(client, toolCall, { response: { output: { sucess: true } } });
             break
           }
@@ -285,24 +382,28 @@ function MementoComponent() {
     };
   }, [client]);
 
-  // const { santitized, scripts } = sanitize(htmlString)
 
   useLayoutEffect(() => {
     if (elRef.current != null) {
-      const range = document.createRange();
-      range.selectNode(elRef.current);
-      const documentFragment = range.createContextualFragment(htmlString);
-
-      // Inject the markup, triggering a re-run! 
-      elRef.current.innerHTML = '';
-      elRef.current.append(documentFragment);
+      const iframe = document.createElement('iframe');
+      iframe.width = "1000px";
+      iframe.height = "600px";
+      iframe.style.border = "none";
+      iframe.srcdoc = htmlString;
+      iframe.id = "html_canvas";
+      // iframe.src = 'data:text/html;charset=utf-8,' + htmlString;
+      elRef.current.appendChild(iframe);
+      console.log("Iframe", iframe);
+      console.log('iframe.contentWindow =', iframe.contentWindow);
+      return () => {
+        elRef.current?.removeChild(iframe);
+      }
     }
-  }, [htmlString]);
+  }, [htmlString])
 
-  // console.log("Executing: ", scripts);
-  // eval(scripts);
-
-  return <div ref={elRef} className="memento" id="memento" dangerouslySetInnerHTML={{ __html: htmlString }} />;
+  return <div>
+    <div ref={elRef} className="memento" id="memento" />
+  </div>;
 }
 
 export const Memento = memo(MementoComponent);
